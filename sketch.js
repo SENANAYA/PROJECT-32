eb47ac7c02225aa1da9e708325accf3843a5e95c
SENANAYA <senagamesbs@gmail.com>
2024-07-30 18:28:54 +0530

    what Bruh

---
+++ sketch.js
@@ -0,0 +1,72 @@
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+
+var engine, world;
+var backgroundImg;
+var hour;
+var minute;
+
+var bg = "sunrise.png";
+
+function preload() {
+    getBackgroundImg();
+    
+}
+
+function setup(){
+    var canvas = createCanvas(windowWidth,windowHeight);
+    engine = Engine.create();
+    world = engine.world;
+
+}
+
+function draw(){
+    if(backgroundImg)
+        background(backgroundImg);
+
+    Engine.update(engine);
+
+    fill("black");
+    textSize(30);
+    
+    if(hour>=12){
+        text("Time : "+ hour%12 +":"+ minute + " PM", 50,100);
+    }else if(hour==0){
+        text("Time : 12 AM",100,100);
+    }else{
+        text("Time : "+ hour%12 + ":"+ minute +" AM", 50,100);
+    }
+
+}
+
+async function getBackgroundImg(){
+
+    // write code to fetch time from API
+    
+    var response = await fetch("http://worldtimeapi.org/api/timezone/Asia/Kolkata")
+    //change the data in JSON format and store it in variable responseJSON
+    var responseJSON = await response.json()
+
+    
+    //fetch datetime from responseJSON
+    var datetime = responseJSON.datetime
+    
+
+    // slice the datetime to extract hour
+     hour = datetime.slice(11,13)
+     minute =datetime.slice(14,16)
+    
+    if(hour>=6 && hour<12 ){
+        bg = "sunrise.png";
+    }else if(hour>=12 && hour <18){
+ bg = "bg.png"
+    }else if(hour >=18 && hour < 19){
+        bg="sunset.png"
+    }else{
+         bg="bg2.jpg"
+    }
+    
+    backgroundImg = loadImage(bg);
+}
